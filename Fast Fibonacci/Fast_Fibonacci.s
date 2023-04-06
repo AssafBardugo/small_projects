7e21c260b8bda084cfa9301081ddf32d879cb73e
Assaf Bardugo <76896663+AssafBardugo@users.noreply.github.com>
2023-04-06 13:35:30 +0300

    Update Fast_Fibonacci.s

--- Fast Fibonacci/Fast_Fibonacci.s
+++ Fast Fibonacci/Fast_Fibonacci.s
@@ -3,7 +3,20 @@
  *	the Fibonacci sequence using Dynamic Programming. 
  *	Complexity: Time O(N), Memory O(1).
  *
- *	The C code of this algorithm is in the file Fast_Fib.c in this folder.
+ *	The C code of this algorithm:
+ **************************************************************
+ *  unsigned long fastFib(unsigned int n){
+ *
+ *      unsigned long A[3] = {0,1,1};
+ *
+ *      for(unsigned int i = 3; i <= n; i++){
+ *
+ *          A[i % 3] = A[(i - 1) % 3] + A[(i - 2) % 3];
+ *      }
+ *
+ *      return A[n % 3];
+ * }
+ **************************************************************
  */
  
 
