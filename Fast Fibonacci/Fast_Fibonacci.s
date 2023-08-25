aa24345771ccddcdf98cb0cd820331964310ccb4
Assaf Bardugo <76896663+AssafBardugo@users.noreply.github.com>
2023-08-25 13:01:37 +0300

    Update Fast_Fibonacci.s

--- Fast Fibonacci/Fast_Fibonacci.s
+++ Fast Fibonacci/Fast_Fibonacci.s
@@ -1,9 +1,3 @@
-/*
- *	This file is an x86 Assembly program that calculates the Nth term of  
- *	the Fibonacci sequence using Dynamic Programming. 
- *	Complexity: Time O(N), Memory O(1).
- */
- 
 
 .global _start
 
